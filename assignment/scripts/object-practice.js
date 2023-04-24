af678d8cd99c5b2f143ccd41ac5fee36175f691e
Dave Nash <davidjnash2@gmail.com>
2023-04-24 12:00:40 -0500

    answered five and added comments

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -25,16 +25,16 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-let fullName = me.firstName + ' ' + me.lastName;
-console.log(fullName);
-
+let fullName = me.firstName + ' ' + me.lastName; //full name variable defined
+console.log(fullName); //log variable
+ 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
 console.log('first favorite food is:', me.favThreeFoods[0], '& last favorite food is:', me.favThreeFoods[me.favThreeFoods.length-1]);
-
+//Above, simultaneously log both first and last foods in array.
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -42,9 +42,9 @@ console.log('first favorite food is:', me.favThreeFoods[0], '& last favorite foo
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-console.log('I have ', me.shoeCount, ' pairs of shoes.');
-me.shoeCount +=1;
-console.log('I now have ', me.shoeCount, ' pairs of shoes.');
+console.log('I have ', me.shoeCount, ' pairs of shoes.'); //show shoes
+me.shoeCount +=1; //add one to shoes
+console.log('I now have ', me.shoeCount, ' pairs of shoes.'); //show new shoes
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -52,3 +52,8 @@ console.log('I now have ', me.shoeCount, ' pairs of shoes.');
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = 'purple'; //adding favorite color to object 'me'
+console.log('My favorite color is: ', me.favoriteColor); //logging result
+
+me.favThreeFoods.push('pizza'); //testing expanding food array
+console.log('I also really love: ', me.favThreeFoods[me.favThreeFoods.length-1]); //logging result
